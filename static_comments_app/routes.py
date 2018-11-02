236fb61887047572d63dbe9d4225019e05351556
Sundaram Vedala <svedala23@gmail.com>
2018-11-02 10:35:43 -0400

    Refactor code, organize into functions for improved readability

--- static_comments_app/routes.py
+++ static_comments_app/routes.py
@@ -6,7 +6,6 @@ import github3
 from datetime import datetime
 import hashlib
 import re
-import os
 
 
 def generate_random_str(length):
@@ -24,22 +23,32 @@ def create_gravatar_hash(email):
     m.update(email.strip().lower().encode("utf-8"))
     return m.hexdigest()
 
-def local_print():
-    # message = request.form['message']
-    # print(":".join("{:02x}".format(ord(c)) for c in message))
+def get_current_datetime_str():
     today_dt = datetime.today()
     date_obj = today_dt.date()
     time_obj = today_dt.time()
     date_str = str(date_obj) + \
         ' {:0>2}:{:0>2}:{:0>2}'.format(time_obj.hour, time_obj.minute, time_obj.second)
-    gravatar_hash = create_gravatar_hash(request.form['email'])
+    return date_str
+
+def website_field_check_scheme():
+    #
+    # Prepend "http://" if no schema present in url entered by the user.
+    #
+    # This is required, since Jekyll treats schema-less url as relative
+    # urls
     if request.form.get('website'):
         website_value = request.form['website']
         if not website_value.startswith('http'):
             website_value = 'http://' + website_value
     else:
         website_value = ''
+    return website_value
+
+def process_message():
     # Message processing
+    #     * This is necessary since we want to write the message as
+    #       YAML's literal string (See the | character written below)
     #     * Replace CR-LF with LF
     #     * Replace beginning of string with two spaces
     #     * Replace a series of newlines with same number of
@@ -49,18 +58,18 @@ def local_print():
     message = re.sub("^", "  ", message)
     pattern = re.compile("(\n+)")
     message = pattern.sub(r'\1  ', message)
-    file_str = 'name: ' + request.form['name'] + '\n'
-    file_str += 'message: |\n'
-    file_str += message + '\n'
+
+    # prepend YAML literal string indicator
+    message = "|\n" + message
+    return message
+
+def generate_file_str(name, message, date_str, gravatar_hash, website_value):
+    file_str = 'name: ' + name + '\n'
+    file_str += 'message: ' + message + '\n'
     file_str += 'date: ' + date_str + '\n'
     file_str += 'gravatar: ' + gravatar_hash + '\n'
     file_str += 'website: ' + website_value + '\n'
-    directory = "~"
-    filename_with_path = os.path.join(
-                            os.path.expanduser(directory),
-                            "submit.yml")
-    f = open(filename_with_path, 'w')
-    f.write(file_str)
+    return file_str
 
 @app.route('/comment/<submitted_token>', methods=["POST"])
 def comments(submitted_token):
@@ -87,55 +96,39 @@ def comments(submitted_token):
         response.headers.add('Access-Control-Allow-Origin', '*')
         return response
 
-    # local_print()
-    # return "OK"
+    date_str = get_current_datetime_str()
+    gravatar_hash = create_gravatar_hash(request.form['email'])
+    website_value = website_field_check_scheme()
+    message = process_message()
 
+    #
+    # Generate data to be written to file and encode it
+    #
+    file_str = generate_file_str(request.form['name'], message, date_str,
+                                gravatar_hash, website_value)
+    content = bytes(file_str, 'utf-8')
+
+    #
+    # Authenticate the github account and get the repository object
+    #
     gh = github3.login(token=github_token)
     repo = gh.repository(github_username, github_repo_name)
 
+    #
     # get sha for latest commit on master branch
+    #
     master_ref = repo.ref('heads/master')
     sha_str = master_ref.object.sha
+
+    #
+    # Create a branch on the repo
+    #
     random_str = generate_random_str(16)
     branch_name = 'refs/heads/jekyll_comments_' + random_str
     repo.create_ref(branch_name, sha_str)
 
-    today_dt = datetime.today()
-    date_obj = today_dt.date()
-    time_obj = today_dt.time()
-    date_str = str(date_obj) + \
-        ' {:0>2}:{:0>2}:{:0>2}'.format(time_obj.hour, time_obj.minute, time_obj.second)
-    gravatar_hash = create_gravatar_hash(request.form['email'])
-    if request.form.get('website'):
-        website_value = request.form['website']
-        if not website_value.startswith('http'):
-            website_value = 'http://' + website_value
-    else:
-        website_value = ''
-
-    # Message processing
-    #     * This is necessary since we want to write the string as
-    #       YAML's literal string (See the | character written below)
-    #     * Replace CR-LF with LF
-    #     * Replace beginning of string with two spaces
-    #     * Replace a series of newlines with same number of
-    #       newlines but two spaces are appended after the last newline
-    message = request.form['message']
-    message = re.sub("\r\n", "\n", message)
-    message = re.sub("^", "  ", message)
-    pattern = re.compile("(\n+)")
-    message = pattern.sub(r'\1  ', message)
-
-    file_str = 'name: ' + request.form['name'] + '\n'
-    file_str += 'message: |\n'
-    file_str += message + '\n'
-    file_str += 'date: ' + date_str + '\n'
-    file_str += 'gravatar: ' + gravatar_hash + '\n'
-    file_str += 'website: ' + website_value + '\n'
-    content = bytes(file_str, 'utf-8')
-
     #
-    # create a file in the just created branch with data from "content"
+    # create a file in the just created branch with data from variable "content"
     #
     file_name = random_str + '.yml'
     full_file_name = '_data/jekyll_comments/' + request.form['slug'] + '/' + file_name
@@ -144,6 +137,9 @@ def comments(submitted_token):
                       content,
                       branch_name)
 
+    #
+    # Create pull request
+    #
     repo.create_pull( 'Adding a comment', 'master',
                       github_username + ':' + branch_name,
                       'This pull request creates a data file to be used as comment')
