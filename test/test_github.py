d37820a9b151be88fcb07ee9d9b79a5263025449
Sundaram Vedala <svedala23@gmail.com>
2018-11-26 13:31:50 -0500

    Test call to generate_random_str

--- test/test_github.py
+++ test/test_github.py
@@ -15,7 +15,6 @@ class GithubTestCase(unittest.TestCase):
     @patch('github3.login')
     def test_github3_repository(self, mock_github3_login):
 
-        sha_str = "my-sha-str"
         github_username = "my-github-username"
         github_repo_name = "my-github-repo-name"
 
@@ -34,10 +33,17 @@ class GithubTestCase(unittest.TestCase):
         mock_ref.assert_called_once_with("heads/master")
 
     @patch('static_comments_app.routes.generate_random_str', return_value="abcdef")
-    def not_ready_yet(self):
+    @patch('github3.login')
+    def test_generate_random_str(self, mock_github3_login,
+                                 mock_generate_random_str):
+        github_username = "my-github-username"
+        github_repo_name = "my-github-repo-name"
+
+        routes.create_github_pull_request("my-github-token",
+            github_username, github_repo_name, "my-slug",
+            "content-line-1\ncontent-line-2\n")
+
         mock_generate_random_str.assert_called_once_with(16)
-        mock_create_ref.assert_called_once_with(branch_name, "abc")
-        pass
 
 if __name__ == '__main__':
     unittest.main()
