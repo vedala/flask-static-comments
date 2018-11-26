53f45b1352d4afea62e3a6c8973850f2baee11ab
Sundaram Vedala <svedala23@gmail.com>
2018-11-26 14:02:38 -0500

    Add test for create_ref

--- test/test_github.py
+++ test/test_github.py
@@ -45,5 +45,33 @@ class GithubTestCase(unittest.TestCase):
 
         mock_generate_random_str.assert_called_once_with(16)
 
+    @patch('static_comments_app.routes.generate_random_str', return_value="abcdef")
+    @patch('github3.login')
+    def test_create_ref(self, mock_github3_login,
+                        mock_generate_random_str):
+
+        github_username = "my-github-username"
+        github_repo_name = "my-github-repo-name"
+
+        mock_repository = MagicMock()
+        mock_github3_login.return_value.repository = mock_repository
+
+        mock_ref = MagicMock()
+        mock_repository.return_value.ref = mock_ref
+
+        sha_str = "my-sha-str"
+        mock_object = MagicMock(sha=sha_str)
+        mock_ref.return_value.object = mock_object
+
+        mock_create_ref = MagicMock()
+        mock_repository.return_value.create_ref = mock_create_ref
+
+        routes.create_github_pull_request("my-github-token",
+            github_username, github_repo_name, "my-slug",
+            "content-line-1\ncontent-line-2\n")
+
+        mock_create_ref.assert_called_once_with(
+            "refs/heads/jekyll_comments_abcdef", sha_str)
+
 if __name__ == '__main__':
     unittest.main()
