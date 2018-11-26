77a55d745151856458e49004e602d395e650cf46
Sundaram Vedala <svedala23@gmail.com>
2018-11-26 11:44:57 -0500

    Add test to test some more code of method create_github_pull_request

--- test/test_github.py
+++ test/test_github.py
@@ -1,5 +1,5 @@
 import unittest
-from unittest.mock import patch
+from unittest.mock import patch, MagicMock
 from static_comments_app import routes
 import github3
 
@@ -11,7 +11,23 @@ class GithubTestCase(unittest.TestCase):
             "my-github-username", "my-github-repo-name", "my-slug",
             "content-line-1\ncontent-line-2\n")
         mock_github3_login.assert_called_once_with(token="my-github-token")
-        pass
+
+    @patch('static_comments_app.routes.generate_random_str', return_value="abcdef")
+    @patch('github3.repository')
+    @patch('github3.login')
+    def test_github3_repository(self, mock_github3_login,
+                                mock_github3_repository, mock_generate_random_str):
+        mock_sha = MagicMock()
+        mock_sha.return_value = "my-sha-str"
+        mock_github3_repository.return_value \
+            .ref.return_value \
+            .object.return_value.sha = mock_sha
+        routes.create_github_pull_request("my-github-token",
+            "my-github-username", "my-github-repo-name", "my-slug",
+            "content-line-1\ncontent-line-2\n")
+        mock_github3_login.assert_called_once_with(token="my-github-token")
+        self.assertEqual("my-sha-str", mock_sha())
+        mock_generate_random_str.assert_called_once_with(16)
 
 if __name__ == '__main__':
     unittest.main()
