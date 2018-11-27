ba07e69d87305a8fa1b4abf2aad4b97ded1db6a3
Sundaram Vedala <svedala23@gmail.com>
2018-11-27 13:19:32 -0500

    Logging only if debug mode not set

--- static_comments_app/__init__.py
+++ static_comments_app/__init__.py
@@ -6,24 +6,25 @@ from logging.handlers import SMTPHandler
 app = Flask(__name__)
 app.config.from_object(Config)
 
-if app.config['LOGGING_EMAIL_NOTIFY'] == 'yes':
-    auth = None
-    secure = None
+if not app.debug:
+    if app.config['LOGGING_EMAIL_NOTIFY'] == 'yes':
+        auth = None
+        secure = None
 
-    if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
-        auth = (app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD'])
+        if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
+            auth = (app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD'])
 
-    if app.config['MAIL_USE_TLS']:
-        secure = ()
+        if app.config['MAIL_USE_TLS']:
+            secure = ()
 
-    mail_handler = SMTPHandler(
-        mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
-        fromaddr='no_reply@flask_static_comments.com',
-        toaddrs=[app.config['LOGGING_EMAIL_TO']],
-        subject='Error encountered in flask static comments',
-        credentials=auth,
-        secure=secure)
-    mail_handler.setLevel(logging.ERROR)
-    app.logger.addHandler(mail_handler)
+        mail_handler = SMTPHandler(
+            mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
+            fromaddr='no_reply@flask_static_comments.com',
+            toaddrs=[app.config['LOGGING_EMAIL_TO']],
+            subject='Error encountered in flask static comments',
+            credentials=auth,
+            secure=secure)
+        mail_handler.setLevel(logging.ERROR)
+        app.logger.addHandler(mail_handler)
 
 from static_comments_app import routes
